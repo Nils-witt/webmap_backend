803b945ba02afe8658074cc7ed4517e2277f8f86
Nils Witt <nils.witt@nils-witt.de>
2026-02-14 19:28:32 +0100

    Added OAuth2 support (2)

--- src/main/java/dev/nilswitt/webmap/entities/repositories/SecurityGroupRepository.java
+++ src/main/java/dev/nilswitt/webmap/entities/repositories/SecurityGroupRepository.java
@@ -4,11 +4,14 @@ import dev.nilswitt.webmap.entities.SecurityGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SecurityGroupRepository extends JpaRepository<SecurityGroup, UUID> {
     Optional<SecurityGroup> findByName(String name);
+
+    List<SecurityGroup> findBySsoGroupName(String ssoGroupName);
 
 }
